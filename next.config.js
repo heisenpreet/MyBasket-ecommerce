1e315499ba3c0b9259900d2d98d86a9ae2c9060f
Harpreet Singh <harpreetsinghsodi152@gmail.com>
2022-08-06 17:12:03 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
 
 module.exports = {
-  images: {
-    domains: ["images.unsplash.com"],
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
